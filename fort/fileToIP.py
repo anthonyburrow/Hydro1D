08b2eda41c84aa744e5dd321b6662cede7843e86
Sarah Stangl <sarah.stangl-1@ou.edu>
2021-05-05 10:14:21 -0500

    changed mass calculation

--- fort/fileToIP.py
+++ fort/fileToIP.py
@@ -19,6 +19,9 @@ xi = data[:,0][:-1]
 # D -- dimensionless parameter used in Caroll and Ostille
 D = data[:,1][:-1]
 
+#change in D
+dDdXI = data[:,2][:-1]
+
 # density, rho = rho_c * D^3
 rho = rho_c * D**3
 
@@ -29,24 +32,35 @@ P = K * rho**((3+1)/3)
 rad = lamb * xi
 
 # mass enclosed -- M = 4 pi lambda^3 rho_c int_{0}^{xi}(xi'^2 * D^n dxi')
-M = (4 * 3.141592 * lamb**3 * xi**3 * D**3) / 3
+#M = (4 * 3.141592 * lamb**3 * xi**3 * D**3) / 3
 
 # no cell centering
-fout = open('full_data.txt','w')
+#fout = open('full_data.txt','w')
 
-fout.write('mass_enclosed, radius, density, pressure\n')
+#fout.write('mass_enclosed, radius, density, pressure\n')
 
-for i in list(range(len(rho))):
-	fout.write(str(M[i])+' '+str(rad[i])+' '+str(rho[i])+' '+str(P[i])+'\n')
+#for i in list(range(len(rho))):
+#	fout.write(str(M[i])+' '+str(rad[i])+' '+str(rho[i])+' '+str(P[i])+'\n')
 
-fout.close()
+#fout.close()
 
 # attempt to center cell 
-fout = open('data.txt','w')
+#fout = open('data.txt','w')
 
+#fout.write('mass, radius, density, pressure\n')
+
+#for i in list(range(len(rho)-1)):
+#	fout.write(str(M[i+1] - M[i])+' '+str((rad[i+1]+rad[i])/2)+' '+str((rho[i+1]+rho[i])/2)+' '+str((P[i+1]+P[i])/2)+'\n')
+
+#fout.close()
+
+M = -4 * 3.141592 * lamb**3 * rho_c * xi**2 * dDdXI
+
+fout = open('data_cell_centered.txt','w')
 fout.write('mass, radius, density, pressure\n')
 
 for i in list(range(len(rho)-1)):
-	fout.write(str(M[i+1] - M[i])+' '+str((rad[i+1]+rad[i])/2)+' '+str((rho[i+1]+rho[i])/2)+' '+str((P[i+1]+P[i])/2)+'\n')
+        fout.write(str(M[i])+' '+str((rad[i+1]+rad[i])/2)+' '+str((rho[i+1]+rho[i])/2)+' '+str((P[i+1]+P[i])/2)+'\n')
 
 fout.close()
+
